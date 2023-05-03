f3916dacf9732bb41b0230aff4961d43c69b4962
Matthew Osborne <momatthewosborne1@gmail.com>
2023-05-03 19:05:39 +0100

    added buttons

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
+console.log(alphabet);
+printBtn = () => {
+  for (i = 0; i < alphabet.length; i++) {
+    var btn = document.createElement("button");
+    var l = document.createTextNode(alphabet[i]);
+    btn.appendChild(l);
+    document.body.appendChild(btn);
+  }
+};
